86d5a4f2c93191686cb578d4ef742f4d63cd13f8
shivam kumar <1thenameisshivam@gmail.com>
2025-02-24 22:20:26 +0530

    isAdmin check added

--- src/Routes/userRoute.js
+++ src/Routes/userRoute.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { registerUser } from "../Controller/userRegister";
+import { authenticateUser } from "../Middleware/authMiddleware";
+import { isAdmin } from "../Middleware/isAdminMiddleware";
 const router = express.Router();
 
-router.post("/register", registerUser);
+router.post("/register", authenticateUser, isAdmin, registerUser);
 
 export default router;
