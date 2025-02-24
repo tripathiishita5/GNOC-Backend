d15a97f07212668e5451d1d4645329040cc17cf8
shivam kumar <1thenameisshivam@gmail.com>
2025-02-24 22:00:55 +0530

    express setup

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import { PORT } from "./config/constant.js";
+import dbConnection from "./config/dbConnection.js";
+import e from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+dbConnection()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB", error);
+    process.exit(1);
+  });
