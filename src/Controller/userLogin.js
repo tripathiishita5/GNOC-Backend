237366ff6f181b93110c929ee218ab9d6367bbd1
shivam kumar <1thenameisshivam@gmail.com>
2025-02-24 22:23:21 +0530

    login user endpoint added

---
+++ src/Controller/userLogin.js
@@ -0,0 +1,32 @@
+import User from "../Models/userSchema.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { SECRET_KEY } from "../config/constant.js";
+
+export const loginUser = async (req, res) => {
+  try {
+    const { empId, password } = req.body;
+
+    const user = await User.findOne({ empId });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ userId: user._id, role: user.role }, SECRET_KEY, {
+      expiresIn: "1y",
+    });
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: { empId, name: user.name, role: user.role },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
--- src/Routes/userRoute.js
+++ src/Routes/userRoute.js
@@ -2,8 +2,10 @@ import express from "express";
 import { registerUser } from "../Controller/userRegister";
 import { authenticateUser } from "../Middleware/authMiddleware";
 import { isAdmin } from "../Middleware/isAdminMiddleware";
+import { loginUser } from "../Controller/userLogin";
 const router = express.Router();
 
 router.post("/register", authenticateUser, isAdmin, registerUser);
+router.post("/login", loginUser);
 
 export default router;
