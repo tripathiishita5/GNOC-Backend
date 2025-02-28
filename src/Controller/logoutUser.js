da9bd10ddfb15d082727bc5b045095d49aa17046
shivam kumar <1thenameisshivam@gmail.com>
2025-02-28 14:25:51 +0530

    add logout api

---
+++ src/Controller/logoutUser.js
@@ -0,0 +1,8 @@
+export const logoutUser = async (req, res) => {
+  try {
+    res.clearCookie("token");
+    res.status(200).json({ message: "Logout successful" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
--- src/Routes/userRoute.js
+++ src/Routes/userRoute.js
@@ -4,10 +4,12 @@ import { authenticateUser } from "../Middleware/authMiddleware.js";
 import { isAdmin } from "../Middleware/isAdminMiddleware.js";
 import { loginUser } from "../Controller/userLogin.js";
 import { selfData } from "../Controller/selfData.js";
+import { logoutUser } from "../Controller/logoutUser.js";
 const router = express.Router();
 
 router.post("/register", authenticateUser, isAdmin, registerUser);
 router.post("/login", loginUser);
 router.get("/profile", authenticateUser, selfData);
+router.post("/logout", logoutUser);
 
 export default router;
